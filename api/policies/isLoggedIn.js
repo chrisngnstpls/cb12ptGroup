e0b9f5ce565e70affcd035e965676921cd798abf
chrisngnstpls <chrisngnstpls@gmail.com>
2021-03-21 22:16:15 +0200

    resetting

--- api/policies/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function(req, res, proceed) {
-    console.log(req.session.user_email)
-    if (req.session.user_email) {
-        return proceed();
-    } 
-    return res.view('pages/unauthorized', {data:'Please login to view this content'})
-}
- 
